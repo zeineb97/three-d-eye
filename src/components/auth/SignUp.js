76adf2b6276419526d0b3a91c14794542d3e922b
meriamtarhouni <37798355+meriamtarhouni@users.noreply.github.com>
2019-04-10 19:09:29 +0100

    form_change_2

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -4,8 +4,7 @@ class SignUp extends Component {
   state = {
     email: '',
     password: '',
-    firstName: '',
-    lastName: '',
+    fullName: '',
     adress: '',
     dateOfBirth:'',
     phone:''
@@ -24,35 +23,39 @@ class SignUp extends Component {
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
           <h3 className="grey-text text-darken-3">Sign Up</h3>
-          <div className="input-field">
+        <div className="row">
+          <div className="input-field"> 
+          <i class="material-icons prefix">email</i>
             <label htmlFor="email">Email</label>
             <input type="email" id='email' onChange={this.handleChange} />
           </div>
           <div className="input-field">
+          <i class="material-icons prefix">edit</i>
             <label htmlFor="password">Password</label>
             <input type="password" id='password' onChange={this.handleChange} />
           </div>
           <div className="input-field">
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" id='firstName' onChange={this.handleChange} />
+          <i class="material-icons prefix">account_circle</i>
+            <label htmlFor="fullName">Full Name</label>
+            <input type="text" id='fullName' onChange={this.handleChange} />
           </div>
+          
           <div className="input-field">
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" id='lastName' onChange={this.handleChange} />
-          </div>
-          <div className="input-field">
+          <i class="material-icons prefix">edit_location</i>
             <label htmlFor="adress">Address</label>
             <input type="text" id='adress' onChange={this.handleChange} />
           </div>
           <div className="input-field">
+          <i class="material-icons prefix">local_phone</i>
             <label htmlFor="phone">Phone Number</label>
             <input type="text" id='phone' onChange={this.handleChange} />
           </div>
-          <div className="input-field">
+          <div >
+          <i class="material-icons prefix">date_range</i>
             <label htmlFor="dateOfBirth">Date Of Birth</label>
             <input type="date" id='dateOfBirth' onChange={this.handleChange} />
           </div>
-
+          </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
           </div>
