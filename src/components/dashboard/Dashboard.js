86da906f6a249fecada5c666f2266de9c45af506
zeineb97 <zeinebchabchoub97@gmail.com>
2019-04-24 23:20:01 +0100

    Cloud Function Auth Trigger

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import {connect } from 'react-redux'
 import {firestoreConnect } from 'react-redux-firebase'
 import {compose } from 'redux'
 import {Redirect} from 'react-router-dom'
+import Notifications from './Notifications'
 class Dashboard extends Component{
     render(){
         
@@ -15,6 +16,9 @@ class Dashboard extends Component{
                 <div className="row"> 
                     <div className="col s12 m5 offset-m1">
                         <GlassesList glasses={glasses} />
+                    </div>
+                    <div className="col s12 m5 offset-m1">
+                         <Notifications />
                     </div>
                 </div>
             </div>
---
+++ src/components/dashboard/Notifications.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Notifications = (props) => {
+  return (
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+          <ul className="online-users">
+            <li>Notification</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Notifications
