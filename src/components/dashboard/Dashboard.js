8481a728bf05dbab080e8eb844fe6ba16b41ea46
zeineb97 <zeinebchabchoub97@gmail.com>
2019-04-24 23:40:48 +0100

    Ordering Firestore Data

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -35,7 +35,7 @@ const mapStateToProps = (state)=> {
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-      { collection: 'glasses' },
-      { collection: 'notifications', limit: 3}
+      { collection: 'glasses' , orderBy: ['createdAt', 'desc'] },
+      { collection: 'notifications', limit: 3 , orderBy: ['time', 'desc']}
     ])
   )(Dashboard)
